c63bf484cce2a93bf146dcdfb3ad900fd3d70dd7
Hue <105862813+Hue19146338@users.noreply.github.com>
2022-06-17 13:29:40 +0700

    Update hue.py

--- hue.py
+++ hue.py
@@ -64,12 +64,12 @@ if selected == "Diagnostic":
             if prediction == 0:
                 st.title("Predicted Label for the image is NORMAL")
                 a = y_pred.max()
-                st.title("Accuracy: ", a \n )
+                st.wirte("**Accuracy:** ", a)
             else:
                 prediction1 = model1.predict(img).argmax()
                 st.title("Predicted Label for the image is {}".format(doan [prediction1]))
                 b = y_pre.max()
-                st.title("Accuracy: " )  st.title(b)
+                st.wirte("**Accuracy:** ", b)
   
 if selected == "More":
     
